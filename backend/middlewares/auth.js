81b1ae1591bd10100b38802d331a28c903758fa8
SeifAllah-Ben-Ahmed <benahmed.seifallah@gmail.com>
2022-06-30 00:08:43 +0200

    auth middleware

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+const AppError = require('../utils/AppError');
+const catchAsync = require('../utils/catchAsync');
+
+exports.authUser = catchAsync(async (req, res, next) => {
+  const tmp = req.header('Authorization');
+  const token = tmp && tmp.split(' ')[1];
+
+  if (!token) {
+    next(
+      new AppError('You are not authorized, Please login to get access', 400)
+    );
+  }
+  const { id } = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  req.user = { id };
+  next();
+});
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const userControllers = require('../controllers/user');
+const auth = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.post('/register', userControllers.register);
 router.post('/activate', userControllers.activateAccount);
 router.post('/login', userControllers.login);
+router.get('/auth', auth.authUser, (req, res) => {
+  res.send('hello world');
+});
 module.exports = router;
